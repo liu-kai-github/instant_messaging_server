7a4da164087f291cd2d40a0fd0e9a5d4d689407c
liu-kai-github <liu-kai-github@users.noreply.github.com>
2018-07-03 17:51:44 +0800

    完善用户注册逻辑

--- methods/user_registered.js
+++ methods/user_registered.js
@@ -1,6 +1,18 @@
 const executeSQL = require('../models/sql');
 
 module.exports = async ([username, password]) => {
+
+    console.log(username, password, 'username, password');
+    const isExist = await executeSQL('SELECT user_id from registered_users WHERE user_id = ?', [username]);
+    console.log(isExist, 'isExist');
+    if (isExist.length !== 0) {
+        return [
+            {
+                message: '该用户名已经存在',
+            }
+        ]
+    }
+
     const result = await executeSQL('INSERT INTO registered_users (user_id, password, registration_time) VALUES (?, ?, ?);', [username, password, Date.now()]);
     console.log(result, 'result');
     return [
