081a241a99618954b84434c99f60b718a54dfcb5
liu-kai-github <liu-kai-github@users.noreply.github.com>
2018-07-05 10:34:15 +0800

    调整代码

--- allMethods.js
+++ allMethods.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // 获取所有服务端在 methods 目录下定义的所有方法
-const allMethods = fs.readdirSync(__dirname + '/' + 'methods')
+const allMethods = fs.readdirSync(`${__dirname}/methods`)
     .filter(
         (f) => {
             return path.extname(f) === '.js';
@@ -10,8 +10,7 @@ const allMethods = fs.readdirSync(__dirname + '/' + 'methods')
     )
     .map(f => {
         const filename = f.replace('.js', '');
-        const requiredFile = require(__dirname + '/' + 'methods/' + f);
-        // console.log(requiredFile.isAuth, 'requiredFile');
+        const requiredFile = require(`${__dirname}/methods/${f}`);
         return {
             [filename]: requiredFile,
         };
