f68b9962c465076e0c5f322cae63fecc386c1481
liu-kai-github <liu-kai-github@users.noreply.github.com>
2018-07-05 11:02:29 +0800

    向带 token 的方法 传入 userID

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 const checkValidity = require('./checkValidity');
 const allMethods = require('./allMethods');
+const executeSQL = require('./models/sql');
 
 /**
  * @description 根据请求体，按照 JSON-RPC 2.0 规则进行相应的响应
@@ -39,7 +40,12 @@ async function singleHandle(requestBody, token) {
         };
     }
 
-    const [err, result] = await allMethods[method](params, token);
+    const userIDResult = await executeSQL('SELECT user_id FROM user_online WHERE session_token = ?', [token]);
+    let userID = '';
+    if (userIDResult.length !== 0) {
+        userID = userIDResult[0].user_id;
+    }
+    const [err, result] = await allMethods[method](params, userID);
     if (err) {
         return {
             error: err,
--- methods/friend_canAddFriends*.js
+++ methods/friend_canAddFriends*.js
@@ -1,7 +1,7 @@
 const executeSQL = require('../models/sql');
 
-module.exports = async ([], token) => {
-    // console.log(token, 'token');
+module.exports = async ([], userID) => {
+    console.log(userID, 'userID');
     const friendsList = await executeSQL('SELECT user_id FROM user_registered;', []);
     console.log(friendsList, 'friendsList');
     return [
