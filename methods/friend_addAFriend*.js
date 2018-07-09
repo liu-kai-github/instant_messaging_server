e2ba5f36a8c9ae00a30e5ad3316e8c85dd505557
liu-kai-github <liu-kai-github@users.noreply.github.com>
2018-07-09 18:55:03 +0800

    申请件夹好友基本开发完成

--- methods/friend_addAFriend*.js
+++ methods/friend_addAFriend*.js
@@ -1,19 +1,33 @@
 const executeSQL = require('../models/sql');
+const {canBeAddedFriends} = require('../server/friend.server');
 
 module.exports = async ([targetUserID], userID) => {
-    const targetUser = executeSQL('SELECT * FROM user_registered WHERE user_id = ?', [targetUserID]);
-    if (targetUser.length !== 1) {
+    const friendsList = await canBeAddedFriends(userID);
+    console.log(friendsList, 'friendsList');
+    if (!friendsList.find(i => i.user_id === targetUserID)) {
         return [
             {
                 message: '添加用户不存在',
             }
-        ]
+        ];
     }
 
-    const alreadyAsked = executeSQL(' SELECT * FROM relation_ask WHERE user_id = ? AND target_id = ?;', [userID, targetUserID]);
+    const alreadyAsked = await executeSQL('SELECT * FROM relation_ask WHERE user_id = ? AND target_id = ?;', [userID, targetUserID]);
+    console.log(alreadyAsked, 'alreadyAsked');
     if (alreadyAsked.length !== 0) {
-
+        await executeSQL('UPDATE relation_ask SET updated_time = ? WHERE user_id = ? AND target_id = ?;', [Date.now(), userID, targetUserID]);
+        return [
+            {
+                message: '曾经添加过该好友',
+            }
+        ];
     }
 
-    executeSQL()
+    await executeSQL('INSERT INTO relation_ask (user_id, target_id, updated_time) VALUES (?, ?, ?)', [userID, targetUserID, Date.now()]);
+    return [
+        null,
+        {
+            message: '已发送请求',
+        }
+    ]
 };
--- methods/friend_canAddFriends*.js
+++ methods/friend_canAddFriends*.js
@@ -2,7 +2,7 @@ const executeSQL = require('../models/sql');
 const {canBeAddedFriends} = require('../server/friend.server');
 
 module.exports = async ([], userID) => {
-    console.log(userID, 'userID');
+    // console.log(userID, 'userID');
     const friendsList = await canBeAddedFriends(userID);
     // console.log(friendsList, 'friendsList');
     return [
